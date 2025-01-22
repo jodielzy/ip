0026551ae779e3208d6d14d4e51fa4142671b2f7
jodielzy <jodielimzhuoying@gmail.com>
2025-01-23 02:16:29 +0800

    Added Lucy.java

---
+++ src/main/java/Lucy.java
@@ -0,0 +1,13 @@
+public class Lucy {
+    public static void main(String[] args) {
+        String line = "_________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm Lucy");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println(line);
+
+    }
+}
